f02553d015e0ff4c6140d1788da5e9ab56bc6ce7
Rohlem <lhoermann@student.tgm.ac.at>
2015-11-06 04:54:54 +0100

    improved style, encapsulated (now returns table)

--- class.lua
+++ class.lua
@@ -1,25 +1,25 @@
-if class == nil then
-  
-  local function first_new(self, obj)
-    self.mt = {__index = self}
-    self.super = getmetatable(self).__index
-    self.new = nil
-    return self:subsequent_new(obj)
-  end
-  
-  local function subsequent_new(self, obj)
-    obj = obj or {}
-    setmetatable(obj, self.mt)
-    obj.new = first_new
-    return obj
-  end
-  
-  local l_class = {
-    first_new = first_new,
-    subsequent_new = subsequent_new,
-    new = subsequent_new
-  }
-  class = l_class
-  l_class.mt = {__index = l_class}
-  
+local getmetatable = getmetatable
+local setmetatable = setmetatable
+
+local function first_new(self, obj)
+  self.mt = {__index = self}
+  self.super = getmetatable(self).__index
+  self.new = nil
+  return self:subsequent_new(obj)
 end
+
+local function subsequent_new(self, obj)
+  obj = obj or {}
+  setmetatable(obj, self.mt)
+  obj.new = first_new
+  return obj
+end
+
+local class = {
+  first_new = first_new,
+  subsequent_new = subsequent_new,
+  new = subsequent_new
+}
+class.mt = {__index = class}
+
+return class
